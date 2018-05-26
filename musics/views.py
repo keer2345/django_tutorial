988e6065338eb491e89e2f247b05473c2dc35de6
keer2345 <keer2345@gmail.com>
2018-05-26 17:53:32 +0800

    TYPE_CHOICES

--- musics/views.py
+++ musics/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from musics.models import Music
+
 
 def hello_view(request):
     hello = "Nice to meet you!"
-    context = {'data': hello}
+    musics = Music.objects.all()
+    context = {'data': hello, 'musics': musics}
     return render(request, 'index.html', context)
