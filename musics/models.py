82157e9e66970c8ab42c84aa6fca8d501194c846
keer2345 <keer2345@gmail.com>
2018-05-27 08:50:08 +0800

    Performing raw queries

--- musics/models.py
+++ musics/models.py
@@ -1,4 +1,6 @@
-from django.db import models
+from collections import namedtuple
+
+from django.db import connection, models
 
 TYPE_CHOICES = (
     ('T1', 'type 1'),
@@ -24,3 +26,48 @@ class Music(models.Model):
 
     def display_type_name(self):
         return self.get_type_display()
+
+
+def fun_raw_sql_query(**kwargs):
+    song = kwargs.get('song')
+    if song:
+        result = Music.objects.raw(
+            'SELECT * FROM musics_music WHERE song = %s', [song])
+    else:
+        result = Music.objects.raw('SELECT * FROM musics_music')
+    return result
+
+
+def namedtuplefetchall(cursor):
+    # Return all rows from a cursor as a namedtuple
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
+
+
+def fun_sql_cursor_update(**kwargs):
+    song = kwargs.get('song')
+    pk = kwargs.get('pk')
+
+    '''
+    Note that if you want to include literal percent signs in the query,
+    you have to double them in the case you are passing parameters:
+    '''
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "UPDATE musics_music SET song = %s WHERE id = %s", [song, pk])
+        cursor.execute("SELECT * FROM musics_music WHERE id = %s", [pk])
+        # result = cursor.fetchone()
+        result = namedtuplefetchall(cursor)
+    result = [
+        {
+            'id': r.id,
+            'song': r.song,
+            'singer': r.singer,
+            'last_modify_date': r.last_modify_date,
+            'created': r.created,
+        }
+        for r in result
+    ]
+
+    return result
--- musics/views.py
+++ musics/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
 
-from musics.models import Music
+from musics.models import Music, fun_raw_sql_query, fun_sql_cursor_update
 from musics.serializers import MusicSerializer
 
 # def hello_view(request):
@@ -14,3 +16,19 @@ from musics.serializers import MusicSerializer
 class MusicViewSet(viewsets.ModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
+
+    # /api/music/raw_sql_query/
+    @list_route(methods=['get'])
+    def raw_sql_query(self, request):
+        song = request.query_params.get('song', None)
+        music = fun_raw_sql_query(song=song)
+        serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # /api/music/{pk}/sql_cursor_update/
+    @detail_route(methods=['put'])
+    def sql_cursor_update(self, request, pk=None):
+        song = request.data.get('song', None)
+        if song:
+            music = fun_sql_cursor_update(song=song, pk=pk)
+            return Response(music, status=status.HTTP_200_OK)
